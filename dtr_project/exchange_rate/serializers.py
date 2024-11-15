05bfe641a8a7073a901a9694dc0a943de4d4e2fc
ilshat kushmanbetov <ilshat26101993@list.ru>
2024-11-15 15:27:13 +0700

    0.2 (views)

---
+++ dtr_project/exchange_rate/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from models import ExchangeRate
+
+class ExchangeRateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ExchangeRate
+        fields = ('date', 'decim')
--- dtr_project/exchange_rate/views.py
+++ dtr_project/exchange_rate/views.py
@@ -1,4 +1,22 @@
-from django.shortcuts import render
-from models import ExchangeRate
 
-def get_current_usd():
+import requests
+#from django.shortcuts import render
+from django.http import JsonResponse
+from models import ExchangeRate
+from serializers import ExchangeRateSerializer
+
+
+def get_current_usd(requests):
+
+    url = 'https://api.exchangeratesapi.io/latest'
+    response = requests.get(url, params={'base': 'USD', 'symbols': 'RUB'})
+    json = response.json()
+    rate_now = json['rates']['RUB']
+
+    ExchangeRate.objects.create(decim=rate_now)
+
+    last_ten_rates = ExchangeRate.objects.order_by('-date')[:10]
+
+    serializer = ExchangeRateSerializer(last_ten_rates, many=True)
+
+    return JsonResponse({'rate_now': rate_now, 'history': serializer.d})
