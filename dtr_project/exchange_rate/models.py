614c91c98efc3f7f41d2ea7988796b0562216adf
ilshat kushmanbetov <ilshat26101993@list.ru>
2024-11-15 20:25:08 +0700

    0.4 (the project is launched)

--- dtr_project/exchange_rate/models.py
+++ dtr_project/exchange_rate/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class ExchangeRate(models.Model):
     date = models.DateField(auto_now_add=True)
-    decim = models.DecimalField(max_digits=10, decimal_places=2)
+    decim = models.DecimalField(max_digits=10, decimal_places=5)
 
     def __str__(self):
         return f'Kурс на {self.date}: {self.decim}'
--- dtr_project/exchange_rate/views.py
+++ dtr_project/exchange_rate/views.py
@@ -2,18 +2,31 @@ import requests
 from django.http import JsonResponse
 from exchange_rate.models import ExchangeRate
 from exchange_rate.serializers import ExchangeRateSerializer
+#from dotenv import load_dotenv
+import os
 
 
 def get_current_usd(request):
-    url = 'https://api.exchangeratesapi.io/latest'
-    response = requests.get(url, params={'base': 'USD', 'symbols': 'RUB'})
-    json = response.json()
-    rate_now = json['rates']['RUB']
+    url = 'https://api.exchangeratesapi.io/v1/latest?access_key=api_key'
+    api_key = os.getenv('API_KEY')
+    response = requests.get(url, params={'base': 'EUR',
+                                         'symbols': 'RUB',
+                                         'access_key': api_key})
+    
+    if response.status_code == 200:
+        json = response.json()
+        rate_now = json.get('rates', {}).get('RUB', None)
 
-    ExchangeRate.objects.create(decim=rate_now)
+        if rate_now is not None:
+            ExchangeRate.objects.create(decim=rate_now)
 
-    last_ten_rates = ExchangeRate.objects.order_by('-date')[:10]
+            last_ten_rates = ExchangeRate.objects.order_by('-date')[:10]
+            serializer = ExchangeRateSerializer(last_ten_rates,
+                                                many=True)
 
-    serializer = ExchangeRateSerializer(last_ten_rates, many=True)
-
-    return JsonResponse({'rate_now': rate_now, 'history': serializer.data})
+            return JsonResponse({'Current exchange rate': rate_now,
+                                 'Query history': serializer.data})
+        else:
+            return JsonResponse({'error': 'Bad Request'}, status=400)
+    else:
+        return JsonResponse({'error': '500'}, status=500)
